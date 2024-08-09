d72fd3dd47e247e67495664b0e6fbdbdbdc36081
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-08-09 14:46:22 -0500

    set bookCoontainer to hold days instead of days and weeks

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -4,29 +4,38 @@ import "../styles/cell.css";
 function BookContainer() {
   const [cellContainer, setCellContainer] = useState([]);
 
-  function cellInfoObj(bookName, Date, PageStarted, PageEnded) {
-    return { bookName, Date, PageStarted, PageEnded };
+  function cellInfoObj(Date) {
+    return { bookName: null, Date, PageStarted: null, PageEnded: null };
   }
 
   useEffect(() => {
-    const numDays = 7;
-    const numWeeks = 52;
+    const currentYear = new Date().getFullYear();
+    const leapYear = currentYear % 4 === 0;
+    const daysInYear = leapYear ? 366 : 365;
 
-    const tempArray = Array.from({ length: numDays }, () =>
-      Array.from({ length: numWeeks }, () => [])
-    );
+    const tempArray = new Array(daysInYear).fill(null).map((_, index) => {
+      const date = new Date(currentYear, 0, index + 1);
+      return cellInfoObj(date.toDateString());
+    });
 
     setCellContainer(tempArray);
   }, []);
 
+  function logInfo(index) {
+    console.log(cellContainer[index]);
+  }
   return (
     <>
       <div className="cellContainer" id="bookContainer">
-        {cellContainer.map((day, dayIndex) =>
-          day.map((week, weekIndex) => (
-            <div key={`${dayIndex}-${weekIndex}`} className="cell"></div>
-          ))
-        )}
+        {cellContainer.map((day, dayIndex) => (
+          <div
+            key={`${dayIndex}`}
+            className="cell"
+            onClick={() => {
+              logInfo(dayIndex);
+            }}
+          ></div>
+        ))}
       </div>
     </>
   );
